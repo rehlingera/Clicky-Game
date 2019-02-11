6d9cdb246f318611054c7d79f20f7348fa754e09
Relliner <41902228+rehlingera@users.noreply.github.com>
2019-02-10 22:32:58 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Clicky-Game/precache-manifest.520f57d11e600dc7497789d6141682fc.js"
+  "/Clicky-Game/precache-manifest.ddb4e2a7dacc85aa0b15d46465e2d20c.js"
 );
 
 workbox.clientsClaim();
